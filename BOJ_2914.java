cd3875ed1965e40ec296f9f62656f60b13c39f1f
kwak-bs <iam-bs@naver.com>
2020-09-17 11:17:53 +0900

    2020.09.17

---
+++ BOJ_2914.java
@@ -0,0 +1,18 @@
+package problemSolve;
+
+import java.util.Scanner;
+
+public class BOJ_2914 {
+	//BOJ / 2914번 / 저작권  / 수학, 구현, 사칙연산 / 브5
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int i = sc.nextInt();
+		
+		System.out.println(a * (i-1) + 1);
+
+	}
+
+}
