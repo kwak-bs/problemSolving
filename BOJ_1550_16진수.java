e850cbafb74f94f98b364605188a9a0fe5737bd7
kwak-bs <iam-bs@naver.com>
2020-09-20 01:07:20 +0900

    2020.09.204

---
+++ BOJ_1550_16진수.java
@@ -0,0 +1,16 @@
+package problemSolve;
+
+import java.util.Scanner;
+
+public class BOJ_1550 {
+	//BOJ / 1550번 /  16진수 / 수학, 구현 / 브5
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input =sc.next();
+		
+		System.out.println(Integer.parseInt(input, 16));
+	}
+
+}
