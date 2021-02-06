dc027f99743b3c8f73eb2612cd3cfd067d9bbb46
kwak-bs <iam-bs@naver.com>
2021-02-06 14:07:41 +0900

    SOL: 2021.02.06 조합

--- Java/KAKAO_2021_BLIND/KAKAO_2021_2.java
+++ Java/KAKAO_2021_BLIND/KAKAO_2021_2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
 
 public class PM_KAKAO_2021_2 {
 	//KAKAO 2021/ 2번 / 메뉴 리뉴얼  
@@ -13,7 +14,7 @@ public class PM_KAKAO_2021_2 {
 		
 		String orders[] = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
 		int course[] = {2,3,4};
-		
+
 		PM_KAKAO_2021_2 c = new PM_KAKAO_2021_2();
 		
 		c.solution(orders, course);
@@ -23,53 +24,54 @@ public class PM_KAKAO_2021_2 {
     public String[] solution(String[] orders, int[] course) {
         String[] answer = {};
         
-        int len[] = new int[course[course.length-1] + 1];
         ArrayList<String> al = new ArrayList<>();
-        
-        for(int i=0; i< orders.length; i++) {
-        	char str[] = orders[i].toCharArray();
-        	Arrays.sort(str);
+		int lenArr[] = new int [course[course.length-1] +1];
+		
+        for(int i=0; i<orders.length; i++) {
+        	char ch[] = orders[i].toCharArray();
+        	Arrays.sort(ch);
         	
         	for(int j=0; j<course.length; j++) {
-        		if(course[j] <= orders[i].length()) {
-        			nCr(str, orders[i].length(), course[j], 0, "");
+        		nCr(ch, orders[i].length(), course[j],0 , "");
+        	}
+        }
+        Set<String> keyset = map.keySet();
+        
+        // 조합의 길이 별로 최댓값 저장.
+        for(String key : keyset) {
+        	if(map.get(key) >= 2) {
+        		if(lenArr[key.length()] < map.get(key)) {
+        			lenArr[key.length()] = map.get(key);
         		}
         	}
         }
         
-        for(String s : map.keySet()) {
-        	if(map.get(s) >= 2) {
-        		if(len[s.length()] < map.get(s)) {
-        			len[s.length()] = map.get(s);
-        		}
+        // 조합의 길이 별로 최댓값과 비교해서 같으면 리스트에 추가.
+        for(String key : keyset) {
+        	if(lenArr[key.length()] == map.get(key)) {
+        		al.add(key);
         	}
         }
-        
-        for(String k: map.keySet()) {
-        	if(2 <= map.get(k) && len[k.length()] == map.get(k)) {
-        		al.add(k);
-        	}
-        }
-        
         Collections.sort(al);
         answer = new String[al.size()];
         
         for(int i=0; i<al.size(); i++) {
         	answer[i] = al.get(i);
-        	System.out.println(answer[i]);
         }
+        
         return answer;
     }
     
-    public static void nCr(char[] str, int n, int r, int start, String result) {
-    	
-    	if( r == 0) {
+    // 조합
+    public static void nCr(char[] ch, int n, int r, int start, String result) {
+    	 
+    	if(r == 0) {
     		map.put(result, map.getOrDefault(result, 0) +1);
     		return;
     	}
     	else {
-    		for(int i=start; i<str.length; i++) {
-    			nCr(str, n, r-1, i+1, result+str[i]);
+    		for(int i=start; i<ch.length; i++) {
+    			nCr(ch, n, r-1, i+1, result+ch[i]);
     		}
     	}
     }
