04621e4bb6d055168648688b27c20c8fd87e6e60
kwak-bs <iam-bs@naver.com>
2021-02-11 13:59:53 +0900

    SOL : 2021.02.11

---
+++ Java/BOJ_15657_N과M(8) (백트래킹).java
@@ -0,0 +1,55 @@
+package problemSolve2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15657 {
+	// BOJ / 15657번 / N과M(8) / 백트래킹 / 실3
+	
+	static int n, m;
+	static int print[], num[];
+
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(stk.nextToken());
+		m = Integer.parseInt(stk.nextToken());
+		
+		num = new int[n];
+		print = new int[m];
+	
+		stk = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<n; i++) {
+			num[i] = Integer.parseInt(stk.nextToken());
+		}
+		Arrays.sort(num);
+		dfs(0,0);
+		
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int depth, int start) {
+		if(depth == m) {
+			for(int value : print) {
+				sb.append(value).append(" ");
+			}
+			
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start; i<n; i++) {
+
+			print[depth] = num[i];
+			dfs(depth+1, i);
+		}
+	}
+}
