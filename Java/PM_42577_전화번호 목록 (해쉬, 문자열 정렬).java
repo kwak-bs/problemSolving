2a685f9c8b21520899ae594bbf846fcb3a439d65
kwak-bs <iam-bs@naver.com>
2021-05-17 17:52:27 +0900

    SOL2 : 2021.05.17 Level2 해쉬, 정렬

--- Java/PM_42577_전화번호 목록 (해쉬, 문자열 정렬).java
+++ Java/PM_42577_전화번호 목록 (해쉬, 문자열 정렬).java
@@ -1,28 +1,25 @@
-package problemSolve4;
+package problemSolve5;
 
 import java.util.Arrays;
 
 public class PM_42577 {
-	// PM / 42577번 / 전화번호 목록 / 해쉬  / Level2
+	// PM / 42577번 / 전화번호 목록 / 해쉬 / Level2
 	public static void main(String[] args) {
-
-		String[] phone_book = {"119", "97674223", "1195524421"};
-		solution(phone_book);
+		solution(new String[] {"119", "97674223", "1195524421"});
 	}
 	
     public static boolean solution(String[] phone_book) {
+        boolean answer = true;
         
         Arrays.sort(phone_book);
-        // 문자열 정렬은 유니코드 순서 기준으로 비교하기 때문에 
-        //문자들이 일치하지만 길이가 다른 경우는 길이가 짧은 문자열이 낮은 순위로 정렬 된다. 
-        //또한, 문자열 비교 특징으로 인해 배열[n]의 문자열을 
-        //접두어로 포함하는 문자열은 배열[n+1]에 위치할 수 있게 된다. 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].indexOf(phone_book[i]) == 0) {
-                return false;
+            // 문자열 정렬은 유니코드 순서 기준으로 비교하기 때문에 문자들이 일치하지만 길이가 다른 경우는 길이가 짧은 문자열이 낮은 순위로 정렬 된다. 또한, 문자열 비교 특징으로 인해 배열[n]의 문자열을 접두어로 포함하는 문자열은 배열[n+1]에 위치할 수 있게 된다. 
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].indexOf(phone_book[i]) == 0) {
+                answer = false;
+                return answer;
             }
         }
+        return answer;
+    }
 
-        return true;
-}
 }
