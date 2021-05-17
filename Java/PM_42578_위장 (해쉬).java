e07492ff8141221fa12e4116cc2b493f3c90bb54
kwak-bs <iam-bs@naver.com>
2021-05-17 18:24:04 +0900

    SOL2 : 2021.05.17 Level2 해쉬, 조합

--- Java/PM_42578_위장 (해쉬).java
+++ Java/PM_42578_위장 (해쉬).java
@@ -1,38 +1,33 @@
-package problemSolve;
+package problemSolve5;
 
 import java.util.HashMap;
-import java.util.Set;
 
 public class PM_42578 {
-	//PM(프로그래머스) / 68644번 / 위장
+	// PM / 42578번 / 위장 / 해쉬, 조합 / Level2
 	public static void main(String[] args) {
-		
-		PM_42578 c = new PM_42578();
-		
-		String clothes [][] = {{"yellow_hat", "headgear"}, 
-					{"blue_sunglasses", "eyewear"}, 
-					{"green_turban", "headgear"}};
-		c.solution(clothes);
+
+		solution(new String[][] {
+			{"yellowhat", "headgear"},
+			{"bluesunglasses", "eyewear"},
+			{"green_turban", "headgear"},
+		});
 	}
 	
-    public int solution(String[][] clothes) {
-        int answer = 1; // 곱셉음 위해 1로 선언
-        HashMap<String, Integer> map = new HashMap<>();
+    public static int solution(String[][] clothes) {
+        int answer = 1;
         
-        for(int i=0; i<clothes.length; i++) {
-        	// 의상 종류, 갯수
-        	map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        HashMap<String, Integer> hm = new HashMap<>();
+        // 종류 별로 갯수를 센다.
+        for(String[] str : clothes) {
+            hm.put(str[1], hm.getOrDefault(str[1],0)+1);
         }
         
-        // 조합
-        Set<String> keySet = map.keySet();
-        
-        for(String key : keySet) {
-        	// 옷 종류마다 안 입는 경우를 +1 해준다.
-        	answer *= map.get(key)+1;
+        // 안 입을 경우를 포함하여 총 조합을 구한다.
+        for(String str : hm.keySet()) {
+            answer *= hm.get(str) + 1;
         }
-        
-         // 아무것도 안 입는 상태 빼주기
+        // 아예 안입는 상태 빼줌
         return answer-1;
     }
+
 }
