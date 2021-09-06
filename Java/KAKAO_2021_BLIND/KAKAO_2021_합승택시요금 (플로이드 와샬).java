0bb9a8e05120bea877acf5e3446b0c3e4607e563
kwak-bs <iam-bs@naver.com>
2021-09-06 18:28:30 +0900

    SOL2 : 2021.09.06 Level3 플로이드 와샬

--- Java/KAKAO_2021_BLIND/KAKAO_2021_합승택시요금 (플로이드 와샬).java
+++ Java/KAKAO_2021_BLIND/KAKAO_2021_합승택시요금 (플로이드 와샬).java
@@ -1,72 +1,60 @@
-package problemSolve3;
+package problemSolve6;
 
-public class KAKAO_2021_합승택시요금 {
-	
+public class PM_72413 {
+	// PM / 72413번 / 합승 택시 요금 / 플로이드 와샬  / Level3 / 2021 KAKAO BLIND
 	public static void main(String[] args) {
-
 		
-		KAKAO_2021_합승택시요금 c = new KAKAO_2021_합승택시요금();
-		int n = 6;
-		int s = 4;
-		int a = 6; 
-		int b = 2;
-		
-		int fares[][] = {
-				{4,1,10},
-				{3,5,24},
-				{5,6,2},
-				{3,1,41},
-				{5,1,24},
-				{4,6,50},
-				{2,4,66},
-				{2,3,22},
-				{1,6,25}
-		};
-		
-		c.solution(n, s, a, b, fares);
+		Solution(6,4,6,2,new int[][] {
+			{4,1,10},
+			{3,5,24},
+			{5,6,2},
+			{3,1,41},
+			{5,1,24},
+			{4,6,50},
+			{2,4,66},
+			{2,3,22},
+			{1,6,25},
+		});
 	}
-	static int [][] dist = new int[200][200];
-	static final int INF = 40000000; 
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = INF;
-        
-        // dist 배열 초기화
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		if( i == j) dist[i][j] = 0;
-        		else dist[i][j] = INF;
-        	}
-        }
-        
-        // fares를 바탕으로 dist 배열 업데이트
-        for(int[] edge : fares) {
-        	dist[edge[0]-1][edge[1]-1] = edge[2];
-        	dist[edge[1]-1][edge[0]-1] = edge[2];
-        }
-        
-        //플로이드 와샬
-        floyd(n);
-        
-        s--;
-        a--;
-        b--;
-        // 완전 탐색
-        for(int k=0; k<n; k++) {
-        	answer = Math.min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
-        }
-        return answer;
-    }
-    
-    public static void floyd(int n) {
-    	
-    	for(int k=0; k<n; k++) { // 경유지
-    		for(int i=0; i<n; i++) { // 시작점 
-    			for(int j=0; j<n; j++) { // 도착점 
-    				if(dist[i][k] + dist[j][k] < dist[i][j]) {
-    					dist[i][j] = dist[i][k] + dist[k][j];
-    				}
-    			}
-    		}
-    	}
-    }
+	static final int INF = 9999999;
+	static int [][] dist;
+	public static int Solution(int n, int s, int a, int b, int[][] fares) {
+		int answer = INF;
+		
+		dist = new int[n+1][n+1];
+		
+		for(int i=1; i<n+1; i++) {
+			for(int j=1; j<n+1; j++) {
+				if(i == j) dist[i][j] = 0;
+				else dist[i][j] = INF;
+			}
+		}
+		
+		for(int i=0; i<fares.length; i++) {
+			dist[fares[i][0]][fares[i][1]] = fares[i][2];
+			dist[fares[i][1]][fares[i][0]] = fares[i][2];
+		}
+		
+		FloydWarshall(n);
+		
+		for(int k=1; k<=n; k++) {
+			answer = Math.min(answer, dist[s][k] + dist[k][a] + dist[k][b]);			
+		}
+		
+		System.out.println(answer);
+		return answer; 
+	}
+	
+	public static void FloydWarshall(int n) {
+		
+		for(int k=1; k<=n; k++) { // 경유지
+			for(int i=1; i<=n; i++) { // 출발지
+				for(int j=1; j<=n; j++) { // 도착지
+					if(dist[i][k] + dist[k][j] < dist[i][j]) {
+						dist[i][j] = dist[i][k] + dist[k][j];
+					}
+				}
+			}
+		} 
+	}
 }
