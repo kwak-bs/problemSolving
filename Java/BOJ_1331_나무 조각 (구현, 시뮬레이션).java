aea3a84f3ff4899e971234d5396b263fd1969427
kwak-bs <iam-bs@naver.com>
2021-12-25 16:34:31 +0900

    SOLVE : 2021.12.25 실버5 구현, 시뮬레이션 문제를 정확히 읽고 문제의 의도대로 해결하면 됐는데 괜히 꼬아서 생각한것가다. 위치를 바꿀때마다 조각의 순서를 출력하는 말이 반복문을 나와서 다시 출력하라는 것인줄 알았다. 어렵게 생각하지 말자

---
+++ Java/BOJ_1331_나무 조각 (구현, 시뮬레이션).java
@@ -0,0 +1,23 @@
+package problemSolve7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1331 {
+	// BOJ / 1331번 / 나이트 투어 / 구현 / 실버5
+	
+	static int dr[] = {1, 1, -1,-1};
+	static int dc[] = {2,-2, 2, -2};
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		for(int i=0; i<36; i++) {
+			String input = br.readLine();
+			
+			
+		}
+	}
+}
