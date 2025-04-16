592e5f7f3415cf6511dbbe18b390257e46771bbf
kwak-bs <iam-bs@naver.com>
2025-04-16 23:43:49 +0900

    SOLVE : 2025.04.16 대소문자 바꿔서 출력하기

---
+++ JavaScript/PM_181949_대소문자 바꿔서 출력하기.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close', function () {
+    str = input[0];
+
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        const ch = str.charAt(i);
+
+        if (ch === ch.toUpperCase()) {
+            result += ch.toLowerCase();
+        } else {
+            result += ch.toUpperCase();
+        }
+    }
+
+    console.log(result);
+});
