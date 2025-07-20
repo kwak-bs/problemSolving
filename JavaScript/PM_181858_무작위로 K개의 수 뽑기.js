7d23831d934318dae345a220e5c50396ea5cd518
kwak-bs <iam-bs@naver.com>
2025-07-20 13:45:02 +0900

    SOLVE : 250720 무작위로 K개의 수 뽑기

---
+++ JavaScript/PM_181858_무작위로 K개의 수 뽑기.js
@@ -0,0 +1,16 @@
+function solution(arr, k) {
+    const answer = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!answer.includes(arr[i])) {
+            answer.push(arr[i]);
+            if (answer.length === k) break;
+        }
+    }
+
+    while (answer.length < k) {
+        answer.push(-1);
+    }
+
+    return answer;
+}
